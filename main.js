9911e2e266b122f3c4477e6eb6efafe84a48aa53
MICHENUY <adriytkachuk162@gmail.com>
2022-11-04 19:26:36 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,102 @@
+let xxx;
+xxx = [12, "book", false, true, 3.14, "pensil", 177, 333, "window", 999]
+console.log(xxx[0])
+console.log(xxx[1])
+console.log(xxx[2])
+console.log(xxx[3])
+console.log(xxx[4])
+console.log(xxx[5])
+console.log(xxx[6])
+console.log(xxx[7])
+console.log(xxx[8])
+console.log(xxx[9])
+
+let bucwar = {title: "Буквар", pageCount: " 40 сторінок", genre: "підручник" };
+console.log(bucwar);
+let koran = {title: "Коран", pageCount: " 233 сторіноки", genre: "релігійна література" };
+console.log(koran);
+let kobzar = {title: "Кобзар", pageCount: " 433 сторінки", genre: "Художня література" };
+console.log(kobzar);
+
+let kapital = {title: "Капітал",
+    pageCount: " 123 сторінки",
+    genre: "Художня література",
+    authors: {
+    name: "Карл Маркс",
+    age: "45 років"}
+}
+console.log(kapital)
+let harri = {
+    title: "Гаппі Поттер",
+    pageCount: "550 сторінок",
+    genre: "Художня література",
+    authors: {
+    name: "Джоан Роулінг",
+    age: " 48 років",
+    }
+}
+console.log(harri)
+let it = {
+    title: "Воно",
+    pageCount: "430 сторінок",
+    genre: "Художня література",
+    authors:{
+        name: "Стівен Кінг",
+        age: "43 роки",
+    }
+}
+console.log(it)
+
+let user = {
+    user_1:{
+        name: "Василь",
+        username: "vasya32425",
+        password: "qwerfhfds",
+    },
+    user_2:{
+        name: "Олег",
+        username: "ol8778787832425",
+        password: "qwerfhfds",
+    },
+    user_3:{
+        name: "Андрій",
+        username: "andre_kaban5",
+        password: "qwerfhfds",
+    },
+    user_4:{
+        name: "Микола ",
+        username: "vaa32425",
+        password: "qwerfhfds",
+    },
+    user_5:{
+        name: "Константин",
+        username: "kiun2425",
+        password: "qwerfhfds",
+    },
+    user_6:{
+        name: "Роберт",
+        username: "vkuupob",
+        password: "qwergvds",
+    },
+    user_7:{
+        name: "Дієго",
+        username: "dirgooos",
+        password: "ituyovhgfuH",
+    },
+    user_8:{
+        name: "Степан",
+        username: "step77832425",
+        password: "qwegvfyus",
+    },
+    user_9:{
+        name: "Володя",
+        username: "546572425",
+        password: "lnluinuo989s",
+    },
+    user_10:{
+        name: "Ігор",
+        username: "igor44345",
+        password: "ipuojp",
+    }
+}
+console.log(user)
